9560ee2f77dc2d4fe6f71a1e89e5dfb6d0ab2bb2
Drew Britt <drewbritt02@gmail.com>
2022-08-05 01:13:42 -0500

    Project Creation

---
+++ code/Game.cs
@@ -0,0 +1,41 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrazyEights;
+
+public partial class Game : Sandbox.Game
+{
+	public Game()
+	{
+	}
+
+	/// <summary>
+	/// A client has joined the server. Make them a pawn to play with
+	/// </summary>
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		// Create a pawn for this client to play with
+		var pawn = new Pawn();
+		client.Pawn = pawn;
+
+		// Get all of the spawnpoints
+		var spawnpoints = Entity.All.OfType<SpawnPoint>();
+
+		// chose a random one
+		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+
+		// if it exists, place the pawn there
+		if ( randomSpawnPoint != null )
+		{
+			var tx = randomSpawnPoint.Transform;
+			tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
+			pawn.Transform = tx;
+		}
+	}
+}
