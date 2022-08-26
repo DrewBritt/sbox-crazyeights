618a7843201d8eebf9223275d1befde0af967727
Drew Britt <drewbritt02@gmail.com>
2022-08-26 18:21:26 -0500

    CardPanel clickable configuration

--- code/UI/Cards/CardPanel.cs
+++ code/UI/Cards/CardPanel.cs
@@ -1,13 +1,42 @@
-﻿using Sandbox;
+﻿using System;
+using Sandbox;
 using Sandbox.UI;
 
 namespace CrazyEights;
 
+/// <summary>
+/// Displays a card in UI
+/// </summary>
 public partial class CardPanel : Panel
 {
-    public CardPanel(Card card)
+    public CardPanel(bool clickable = false)
     {
         StyleSheet.Load("/UI/Cards/CardPanel.scss");
-        Style.BackgroundImage = Texture.Load(FileSystem.Mounted, card.FileName);
+
+        // Setup OnClick if clickable
+        if(!clickable) return;
+
+        // No card = draw pile card
+        AddEventListener("onclick", () => ConsoleSystem.Run("crazyeights_drawcard"));
+    }
+
+    public CardPanel(Card c, bool clickable = false)
+    {
+        StyleSheet.Load("/UI/Cards/CardPanel.scss");
+        SetCard(c);
+
+        // Setup OnClick if clickable
+        if(!clickable) return;
+
+        /*if(card.Suit == CardSuit.Wild)
+         * AddEventListener("onclick", () => ToggleWildSelection()); 
+           return;  */
+
+        AddEventListener("onclick", () => ConsoleSystem.Run($"crazyeights_playcard {c.NetworkIdent}"));
+    }
+
+    public void SetCard(Card c)
+    {
+        Style.BackgroundImage = Texture.Load(FileSystem.Mounted, c.FileName);
     }
 }
