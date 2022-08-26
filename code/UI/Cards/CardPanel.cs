bf0fe4d22c0b812abd9e527fe228255d5ce78c47
Drew Britt <drewbritt02@gmail.com>
2022-08-25 22:25:58 -0500

    More UI shit

---
+++ code/UI/Cards/CardPanel.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace CrazyEights;
+
+public partial class CardPanel : Panel
+{
+    public CardPanel()
+    {
+        StyleSheet.Load("UI/CardPanel.scss");
+    }
+}
---
+++ code/UI/Cards/PlayerHand.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace CrazyEights;
+
+public partial class PlayerHand : Panel
+{
+    public PlayerHand()
+    {
+        StyleSheet.Load("UI/PlayerHand.scss");
+    }
+}
