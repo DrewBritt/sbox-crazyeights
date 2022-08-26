ec0232ebab5d1fba1902b36ed99f5e5e4a8f0984
Drew Britt <drewbritt02@gmail.com>
2022-08-26 18:22:00 -0500

    TableCards to display last played card and draw pile

---
+++ code/UI/Cards/TableCards.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace CrazyEights;
+
+public partial class TableCards : Panel
+{
+    public Panel DrawPile { get; set; }
+    public Panel PlayPile { get; set; }
+
+    private CardPanel playCard;
+
+    public TableCards()
+    {
+        SetTemplate("Code/UI/Cards/TableCards.html");
+
+        DrawPile.AddChild(new CardPanel(true));
+
+        playCard = new CardPanel(false);
+        PlayPile.AddChild(playCard);
+    }
+
+    public void SetPlayCard(Card c)
+    {
+        playCard.SetCard(c);
+    }
+}
