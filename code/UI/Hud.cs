0960b4043cdd807eea0bcb2299b8227bfc833103
Drew Britt <drewbritt02@gmail.com>
2022-08-26 19:52:03 -0500

    Integrate SuitSelectionOverlay (fully playable!)

--- code/UI/Hud.cs
+++ code/UI/Hud.cs
@@ -6,10 +6,16 @@ namespace CrazyEights;
 [UseTemplate]
 public partial class Hud : RootPanel
 {
-    public TableCards TableCards { get; set; }
+    private TableCards TableCards { get; set; }
+    private SuitSelectionOverlay SuitSelectionOverlay { get; set; }
 
     public void UpdatePlayedCard()
     {
         TableCards.SetPlayCard(Game.Current.PlayingPile.GetTopCard());
+    }
+
+    public void OpenSuitSelection(Card c)
+    {
+        SuitSelectionOverlay.OpenPanel(c);
     }
 }
