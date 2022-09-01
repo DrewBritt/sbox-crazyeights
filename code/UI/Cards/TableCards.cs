2ca4472dc07651a59d572d0f7dc63f01be2f23d0
Drew Britt <drewbritt02@gmail.com>
2022-09-01 00:12:42 -0500

    TableCards appears only when player has cards/playing

--- code/UI/Cards/TableCards.cs
+++ code/UI/Cards/TableCards.cs
@@ -14,6 +14,8 @@ public partial class TableCards : Panel
     {
         SetTemplate("Code/UI/Cards/TableCards.html");
 
+        BindClass("open", () => (Local.Pawn as Pawn).Hand.Cards.Count > 0);
+
         var drawCard = new CardPanel();
         drawCard.AddEventListener("onclick", () => ConsoleSystem.Run("crazyeights_drawcard"));
         DrawPile.AddChild(drawCard);
