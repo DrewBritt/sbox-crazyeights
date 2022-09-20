a1fc007d475a5492b14f260128d6b29d2e57aa50
Drew Britt <drewbritt02@gmail.com>
2022-09-19 19:47:34 -0500

    Create CardEntity, is physical model representation of a card (in players hand)

---
+++ code/Cards/CardEntity.cs
@@ -0,0 +1,43 @@
+﻿using Sandbox;
+
+namespace CrazyEights;
+
+public partial class CardEntity : ModelEntity
+{
+    public Card Card { get; set; }
+
+    private Texture tex;
+    private Material mat;
+
+    public CardRank Rank => Card.Rank;
+    public CardSuit Suit => Card.Suit;
+    public bool IsPlayable() => Card.IsPlayable();
+
+    public override void Spawn()
+    {
+        base.Spawn();
+
+        SetModel("models/card/card.vmdl");
+        Transmit = TransmitType.Always;
+    }
+
+    [ClientRpc]
+    public void SetCard(CardRank rank, CardSuit suit)
+    {
+        Card = new Card()
+        {
+            Rank = rank,
+            Suit = suit
+        };
+        tex = Texture.Load(FileSystem.Mounted, Card.FileName);
+        mat = Material.Load("materials/card/card.vmat").CreateCopy();
+        SetMaterialOverride(mat, "isTarget");
+    }
+
+    [Event.Frame]
+    public void OnFrame()
+    {
+        if(tex?.IsLoaded ?? false)
+            mat.OverrideTexture("Color", tex);
+    }
+}
