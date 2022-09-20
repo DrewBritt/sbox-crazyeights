ceee5d7740d6da0b4257641ff931072be0768c12
Drew Britt <drewbritt02@gmail.com>
2022-09-20 02:00:11 -0500

    GameTable entity to place in map, pile/deck positions relative to this

---
+++ code/Map/GameTable.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Sandbox;
+using SandboxEditor;
+
+namespace CrazyEights;
+
+[Model]
+[SupportsSolid]
+[Library("ce_table")]
+[Display(Name = "Crazy Eights Table", Description = "A table in which cards will appear on.")]
+[HammerEntity]
+public partial class GameTable : ModelEntity
+{
+    public override void Spawn()
+    {
+        base.Spawn();
+
+        SetupPhysicsFromModel(PhysicsMotionType.Static);
+        EnableAllCollisions = true;
+        EnableDrawing = true;
+    }
+}
