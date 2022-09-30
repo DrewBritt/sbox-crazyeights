d67d01fd09bb2a7a1b37df453be6cc54ffde3049
Drew Britt <drewbritt02@gmail.com>
2022-09-29 22:50:38 -0500

    Initial settings/admin panel

---
+++ code/UI/Settings/Settings.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace CrazyEights;
+
+[UseTemplate]
+public partial class Settings : NavPanel
+{
+    public Settings()
+    {
+        Navigate("settings/gamesettings");
+    }
+
+    [Event.BuildInput]
+    public void BuildInput(InputBuilder b)
+    {
+        if(b.Pressed(InputButton.Menu) && ConsoleSystem.GetValue("sv_cheats") == "1")
+            SetClass("open", !HasClass("open"));
+    }
+}
