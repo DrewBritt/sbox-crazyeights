90c79cfca9a97241ac9d6d8915b51c976ce7bbfb
Drew Britt <drewbritt02@gmail.com>
2025-02-14 23:41:45 -0600

    NetworkManager

---
+++ Code/Common/NetworkManager.cs
@@ -0,0 +1,28 @@
+﻿namespace CrazyEights.Common;
+
+public sealed class NetworkManager : Component, Component.INetworkListener
+{
+	[Property] public GameObject PlayerPrefab { get; set; }
+
+	protected override void OnStart()
+	{
+		if ( Networking.IsActive ) return;
+
+		var lobbyConfig = new LobbyConfig
+		{
+			DestroyWhenHostLeaves = false,
+			AutoSwitchToBestHost = true,
+			MaxPlayers = 8,
+			Privacy = LobbyPrivacy.Private,
+		};
+		Networking.CreateLobby( lobbyConfig );
+	}
+
+	public void OnActive( Connection connection )
+	{
+		var player = PlayerPrefab.Clone();
+		player.Name = $"Player - {connection.DisplayName}";
+		player.Network.SetOwnerTransfer( OwnerTransfer.Fixed );
+		player.NetworkSpawn( connection );
+	}
+}
---
+++ Code/Usings.cs
@@ -0,0 +1,8 @@
+﻿global using Sandbox;
+global using Sandbox.Diagnostics;
+global using Sandbox.Network;
+global using Sandbox.UI;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
